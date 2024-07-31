d0cd9483c6cccfffd64f1ba550092f9411558c47
XVI-IX <oladoja14@gmail.com>
2024-07-31 22:13:41 +0100

    minor adjustments

--- packages/gateway/src/app.controller.ts
+++ packages/gateway/src/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    @Inject('AUTH_SERVICE') private readonly authClient: ClientProxy,
+    @Inject('EMPLOYEE_SERVICE') private readonly employeeClient: ClientProxy,
+    @Inject('PROJECTS_SERVICE') private readonly projectsClient: ClientProxy,
+  ) {}
 
-  @Get()
-  getHello(): string {
+  @Post('register')
+  register(@Body() dto: any): string {
     return this.appService.getHello();
   }
 }
--- packages/gateway/src/main.ts
+++ packages/gateway/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     {
-      transport: Transport.TCP,
+      transport: Transport.RMQ,
     },
   );
   await app.listen();
