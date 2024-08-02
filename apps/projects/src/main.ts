79b6dfbb1b1e12c5e29471ffb1b8d3da0a9e808e
XVI-IX <oladoja14@gmail.com>
2024-08-02 17:07:10 +0100

    update

--- apps/projects/src/main.ts
+++ apps/projects/src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ProjectsModule } from './projects.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(ProjectsModule);
   await app.listen(3000);
 }
 bootstrap();
