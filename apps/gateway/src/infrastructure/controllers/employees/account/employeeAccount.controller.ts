ad447f819934a7d0f857fca90d1c1d1fe50cb1a2
David-Daniel Ojebiyi <oladoja14@gmail.com>
2024-11-25 16:35:42 +0100

    feat (gateway) - add controllers for employee retrieval and updates

--- apps/gateway/src/infrastructure/controllers/employees/account/employeeAccount.controller.ts
+++ apps/gateway/src/infrastructure/controllers/employees/account/employeeAccount.controller.ts
@@ -1,6 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Put, Query } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { UpdateEmployeeInput } from 'apps/employees/src/infrastructure/common/schemas/account.schema';
 
 @Controller('api/v1/employee/account')
 export class EmployeeAccountController {
-  constructor() {}
+  constructor(
+    @Inject('EMPLOYEE_SERVICE') private readonly employeeService: ClientProxy,
+  ) {}
+
+  @Get()
+  async getAllEmployees() {
+    return this.employeeService.send('getAllEmployees', {});
+  }
+
+  @Get('/:employeeId')
+  async getEmployeeById(@Query('employeeId') employeeId: string) {
+    return this.employeeService.send('getEmployeeById', { employeeId });
+  }
+
+  @Put('/:employeeId')
+  async updateEmployee(
+    @Query('employeeId') employeeId: string,
+    @Body() data: UpdateEmployeeInput,
+  ) {
+    return this.employeeService.send('updateEmployee', { employeeId, ...data });
+  }
 }
