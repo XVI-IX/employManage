d35957506e36f112ad5a37e439fe2c7ed24f6faf
David-Daniel Ojebiyi <oladoja14@gmail.com>
2025-01-06 20:29:47 +0100

    feat (project.controller) - add controller methods

--- apps/gateway/src/infrastructure/controllers/projects/projects.controller.ts
+++ apps/gateway/src/infrastructure/controllers/projects/projects.controller.ts
@@ -1,5 +1,14 @@
-import { Body, Controller, Inject, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Inject,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
+import { ProjectModel } from 'apps/projects/src/domain/model';
 import { CreateProjectInput } from 'apps/projects/src/infrastructure/common/schemas/createProjectInput.schema';
 
 @Controller('api/v1/projects')
@@ -11,5 +20,26 @@ export class ProjectsGatewayController {
   @Post('/')
   async createProject(@Body() data: CreateProjectInput) {
     return this.projectsService.send('createProject', data);
+  }
+
+  @Get('/')
+  async getAllProjects() {
+    return this.projectsService.send('getAllProjects', {});
+  }
+
+  @Get('/:projectId')
+  async getProjectById(@Param('projectId') projectId: string) {
+    return this.projectsService.send('getProjectById', { projectId });
+  }
+
+  @Put('/:projectId')
+  async updateProject(
+    @Param('projectId') projectId: string,
+    @Body() data: Partial<ProjectModel>,
+  ) {
+    return this.projectsService.send('updateProject', {
+      projectId,
+      data,
+    });
   }
 }
--- apps/projects/src/infrastructure/controllers/projects.controller.ts
+++ apps/projects/src/infrastructure/controllers/projects.controller.ts
@@ -1,16 +1,27 @@
 import { Controller, Inject } from '@nestjs/common';
-import { MessagePattern } from '@nestjs/microservices';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 import { CreateProjectInput } from '../common/schemas/createProjectInput.schema';
 import { UseCaseProxy } from '@app/common/infrastructure/usecase-proxy/usecase-proxy';
 import { CreateProjectUseCase } from '../../usecase/createProject.usecase';
 import { ProjectsGeneralUseCaseProxy } from '../usecase-proxy/projectsGeneralUseCase.proxy';
 import { HttpResponse } from '@app/common/infrastructure/helpers/response.helper';
+import { GetAllProjectsUseCase } from '../../usecase/getAllProjects.usecase';
+import { GetProjectByIdUseCase } from '../../usecase/getProjectById.usecase';
+import { UpdateProjectUseCase } from '../../usecase/updateProject.usecase';
 
 @Controller()
 export class ProjectsController {
   constructor(
     @Inject(ProjectsGeneralUseCaseProxy.CREATE_PROJECT_USE_CASE_PROXY)
     private readonly createProjectUseCase: UseCaseProxy<CreateProjectUseCase>,
+    @Inject(ProjectsGeneralUseCaseProxy.GET_ALL_PROJECTS_USE_CASE_PROXY)
+    private readonly getAllProjectUseCase: UseCaseProxy<GetAllProjectsUseCase>,
+    @Inject(
+      ProjectsGeneralUseCaseProxy.GET_PROJECTS_BY_DEPARTMENT_ID_USE_CASE_PROXY,
+    )
+    private readonly getProjectByIdUseCase: UseCaseProxy<GetProjectByIdUseCase>,
+    @Inject(ProjectsGeneralUseCaseProxy.UPDATE_PROJECT_USE_CASE_PROXY)
+    private readonly updateProjectUseCase: UseCaseProxy<UpdateProjectUseCase>,
   ) {}
 
   @MessagePattern('createProject')
@@ -20,5 +31,32 @@ export class ProjectsController {
       .createProject(data);
 
     return HttpResponse.send('Project created successfully', response);
+  }
+
+  @MessagePattern('getAllProjects')
+  async getAllProjects() {
+    const response = await this.getAllProjectUseCase
+      .getInstance()
+      .getAllProjects();
+
+    return HttpResponse.send('Projects retrieved successfully', response);
+  }
+
+  @MessagePattern('getProjectById')
+  async getProjectById(@Payload() data: { projectId: string }) {
+    const response = await this.getProjectByIdUseCase
+      .getInstance()
+      .getProjectById(data.projectId);
+
+    return HttpResponse.send('Project retrieved successfully', response);
+  }
+
+  @MessagePattern('updateProject')
+  async updateProject(@Payload() data: { projectId: string; data: any }) {
+    const response = await this.updateProjectUseCase
+      .getInstance()
+      .updateProject(data.projectId, data.data);
+
+    return HttpResponse.send('Project updated successfully', response);
   }
 }
