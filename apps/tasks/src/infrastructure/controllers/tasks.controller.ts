3c6ebc33665a75718dfdf1a5d39baf83e1ae0283
David-Daniel Ojebiyi <oladoja14@gmail.com>
2025-01-12 19:04:45 +0100

    feat (task controller) - add methods to tasks controller in task app

--- apps/tasks/src/infrastructure/controllers/tasks.controller.ts
+++ apps/tasks/src/infrastructure/controllers/tasks.controller.ts
@@ -5,17 +5,109 @@ import { CreateTaskUseCase } from '../../usecases/createTask.usecase';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 import { CreateTaskInput } from '../common/schemas/tasks.schema';
 import { HttpResponse } from '@app/common/infrastructure/helpers/response.helper';
+import { GetAllTasksUseCase } from '../../usecases/getAllTasks.usecase';
+import { GetTaskByIdUseCase } from '../../usecases/getTaskById.usecase';
+import { TasksModel } from '../../domain/models/tasks.model';
+import { UpdateTaskUseCase } from '../../usecases/updateTask.usecase';
+import { DeleteTaskUseCase } from '../../usecases/deleteTasks.usecase';
+import { GetTaskByProjectIdUseCase } from '../../usecases/getTaskByProjectId.usecase';
+import { GetTasksByProjectIdAndStatus } from '../../usecases/getTasksByProjectIdAndStatus.usecase';
+import { GetTasksByEmployeeId } from '../../usecases/getTasksByEmployeeId.usecase';
 
 @Controller()
 export class TasksController {
-  constructor(@Inject(TasksGeneralUsecaseProxyModule.CREATE_TASK_USE_CASE_PROXY) private readonly createTaskUseCase: UseCaseProxy<CreateTaskUseCase>) {}
+  constructor(
+    @Inject(TasksGeneralUsecaseProxyModule.CREATE_TASK_USE_CASE_PROXY)
+    private readonly createTaskUseCase: UseCaseProxy<CreateTaskUseCase>,
+    @Inject(TasksGeneralUsecaseProxyModule.GET_ALL_TASKS_USE_CASE_PROXY)
+    private readonly getAllTasksUseCase: UseCaseProxy<GetAllTasksUseCase>,
+    private readonly getTaskByIdUseCase: UseCaseProxy<GetTaskByIdUseCase>,
+    @Inject(TasksGeneralUsecaseProxyModule.UPDATE_TASK_USE_CASE_PROXY)
+    private readonly updateTaskUseCase: UseCaseProxy<UpdateTaskUseCase>,
+    @Inject(TasksGeneralUsecaseProxyModule.DELETE_TASK_USE_CASE_PROXY)
+    private readonly deleteTaskUseCase: UseCaseProxy<DeleteTaskUseCase>,
+    @Inject(
+      TasksGeneralUsecaseProxyModule.GET_TASKS_BY_PROJECT_ID_USE_CASE_PROXY,
+    )
+    private readonly getTasksByProjectIdUseCase: UseCaseProxy<GetTaskByProjectIdUseCase>,
+    // @Inject(TasksGeneralUsecaseProxyModule.GET_TASKS_BY_DEPARTMENT_ID_USE_CASE_PROXY) private readonly getTasksByDepartmentIdUseCase: UseCaseProxy<GetTask>,
+    @Inject(
+      TasksGeneralUsecaseProxyModule.GET_TASKS_BY_PROJECT_ID_AND_EMPLOYEE_ID_AND_STATUS_USE_CASE_PROXY,
+    )
+    private readonly getTasksByProjectIdAndEmployeeIdAndStatusUseCase: UseCaseProxy<GetTasksByProjectIdAndStatus>,
+    @Inject(
+      TasksGeneralUsecaseProxyModule.GET_TASKS_BY_EMPLOYEE_ID_USE_CASE_PROXY,
+    )
+    private readonly getTasksByEmployeeIdUseCase: UseCaseProxy<GetTasksByEmployeeId>,
+  ) {}
 
   @MessagePattern('createTask')
-  async createTask(@Payload() data: CreateTaskInput ) {
-    const task = await this.createTaskUseCase.getInstance().createTaskRepository(data);
+  async createTask(@Payload() data: CreateTaskInput) {
+    const task = await this.createTaskUseCase
+      .getInstance()
+      .createTaskRepository(data);
 
     return HttpResponse.send('Task created', task);
   }
 
-  
+  @MessagePattern('getAllTasks')
+  async getAllTasks() {
+    const tasks = await this.getAllTasksUseCase
+      .getInstance()
+      .getAllTasksUseCase();
+
+    return HttpResponse.send('Tasks fetched', tasks);
+  }
+
+  @MessagePattern('getTaskById')
+  async getTaskById(@Payload() data: { taskId: string }) {
+    const task = await this.getTaskByIdUseCase
+      .getInstance()
+      .getTaskById(data.taskId);
+
+    return HttpResponse.send('Task Retrieved', task);
+  }
+
+  @MessagePattern('updateTask')
+  async updateTask(
+    @Payload() data: { taskId: string; data: Partial<TasksModel> },
+  ) {
+    const task = await this.updateTaskUseCase
+      .getInstance()
+      .updateTask(data.taskId, data.data);
+
+    return HttpResponse.send('Task updated', task);
+  }
+
+  @MessagePattern('deleteTask')
+  async deleteTask(@Payload() data: { taskId: string }) {
+    await this.deleteTaskUseCase.getInstance().deleteTask(data.taskId);
+
+    return HttpResponse.send('Task deleted', {});
+  }
+
+  @MessagePattern('getTaskByProjectId')
+  async getTasksByProjectId(@Payload() data: { projectId: string }) {
+    const tasks = await this.getTasksByProjectIdUseCase
+      .getInstance()
+      .getTaskByProjectId(data.projectId);
+
+    return HttpResponse.send('Tasks fetched', tasks);
+  }
+
+  @MessagePattern('getTasksByProjectIdAndEmployeeIdAndStatus')
+  async getTasksByProjectIdAndEmployeeIdAndStatus() {
+    // const tasks = await this.getTasksByProjectIdAndEmployeeIdAndStatusUseCase.getInstance().getTasksByProjectIdAndStatus();
+
+    return HttpResponse.send('Error with implementation', {});
+  }
+
+  @MessagePattern('getTasksByEmployeeId')
+  async getTasksByEmployeeId(@Payload() data: { employeeId: string }) {
+    const tasks = await this.getTasksByEmployeeIdUseCase
+      .getInstance()
+      .GetTasksByEmployeeId(data.employeeId);
+
+    return HttpResponse.send('Tasks fetched', tasks);
+  }
 }
--- apps/tasks/src/usecases/updateTask.usecase.ts
+++ apps/tasks/src/usecases/updateTask.usecase.ts
@@ -4,10 +4,7 @@ import { ITaskRepository } from '../domain/repositories/tasks.repository';
 export class UpdateTaskUseCase {
   constructor(private readonly taskRepository: ITaskRepository) {}
 
-  async updateTaskRepository(
-    id: string,
-    data: Partial<TasksModel>,
-  ): Promise<TasksModel> {
+  async updateTask(id: string, data: Partial<TasksModel>): Promise<TasksModel> {
     await this.taskRepository.update(id, data);
 
     const task = await this.taskRepository.findOne({
