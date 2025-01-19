a8670a970b4536d20324d4b76219eba97a6694ae
David-Daniel Ojebiyi <oladoja14@gmail.com>
2025-01-19 03:59:10 +0100

    feat (attendance gateway controller) - update error handling

--- apps/gateway/src/infrastructure/controllers/attendance/attendance.controller.ts
+++ apps/gateway/src/infrastructure/controllers/attendance/attendance.controller.ts
@@ -26,34 +26,54 @@ export class AttendanceGatewayController {
   @Public()
   @Post('/')
   async createAttendance(@Body() data: CreateAttendanceInput) {
-    return this.attendanceService.send('createAttendance', data);
+    try {
+      return this.attendanceService.send('createAttendance', data);
+    } catch (error) {
+      throw error;
+    }
   }
 
   @Get('/')
   async getAllAttendances() {
-    return this.attendanceService.send('getAllAttendances', {});
+    try {
+      return this.attendanceService.send('getAllAttendances', {});
+    } catch (error) {
+      throw error;
+    }
   }
 
   @Get('/:attendanceId')
   async getAttendanceById(@Param('attendanceId') attendanceId: string) {
-    return this.attendanceService.send('getAttendanceById', { attendanceId });
+    try {
+      return this.attendanceService.send('getAttendanceById', { attendanceId });
+    } catch (error) {
+      throw error;
+    }
   }
 
   @Public()
   @Put('/')
   async updateAttendance(@Body() data: CheckOutInput) {
-    return this.attendanceService.send('checkout', {
-      employeeId: data.employeeId,
-      ...data,
-    });
+    try {
+      return this.attendanceService.send('checkout', {
+        employeeId: data.employeeId,
+        ...data,
+      });
+    } catch (error) {
+      throw error;
+    }
   }
 
   @Get('/all/date-range')
   async getAttendanceByDateRange(
     @Query() qObj: { start?: string; end?: string },
   ) {
-    console.log(qObj);
-    return this.attendanceService.send('getAttendanceByDateRange', qObj);
+    try {
+      console.log(qObj);
+      return this.attendanceService.send('getAttendanceByDateRange', qObj);
+    } catch (error) {
+      throw error;
+    }
   }
 
   @Get('/employees/:employeeId')
@@ -66,34 +86,45 @@ export class AttendanceGatewayController {
     },
     @Param('employeeId') employeeId: string,
   ) {
-    if (qObj?.date) {
-      return this.attendanceService.send('getAttendanceByEmployeeIdAndDate', {
-        employeeId,
-        date: qObj.date,
-      });
-    } else if (qObj?.month && qObj.year) {
-      return this.attendanceService.send(
-        'getAttendanceByEmployeeIdAndMonthAndYear',
-        {
+    try {
+      if (qObj?.date) {
+        return this.attendanceService.send('getAttendanceByEmployeeIdAndDate', {
           employeeId,
-          month: qObj.month,
-          year: qObj.year,
-        },
-      );
-    } else if (qObj?.month) {
-      return this.attendanceService.send('getAttendanceByEmployeeIdAndMonth', {
-        employeeId,
-        month: qObj.month,
-      });
-    } else {
-      return this.attendanceService.send('getAttendanceByEmployeeId', {
-        employeeId,
-      });
+          date: qObj.date,
+        });
+      } else if (qObj?.month && qObj.year) {
+        return this.attendanceService.send(
+          'getAttendanceByEmployeeIdAndMonthAndYear',
+          {
+            employeeId,
+            month: qObj.month,
+            year: qObj.year,
+          },
+        );
+      } else if (qObj?.month) {
+        return this.attendanceService.send(
+          'getAttendanceByEmployeeIdAndMonth',
+          {
+            employeeId,
+            month: qObj.month,
+          },
+        );
+      } else {
+        return this.attendanceService.send('getAttendanceByEmployeeId', {
+          employeeId,
+        });
+      }
+    } catch (error) {
+      throw error;
     }
   }
 
   @Delete('/:attendanceId')
   async deleteAttendance(@Param('attendanceId') attendanceId: string) {
-    return this.attendanceService.send('deleteAttendance', { attendanceId });
+    try {
+      return this.attendanceService.send('deleteAttendance', { attendanceId });
+    } catch (error) {
+      throw error;
+    }
   }
 }
